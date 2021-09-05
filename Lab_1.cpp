d47237e871fb8c6b428ea6da83411faaa1b63292
Conner Oaks <conoaks@gmail.com>
2021-09-05 12:35:41 -0400

    Lab1

---
+++ Lab_1.cpp
@@ -0,0 +1,12 @@
+//Conner Oaks 9/5/2021 Lab 1
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<< "********************\n";
+    cout<<"Welcome to the Lab_1 Program\n";
+    cout<<"Program Description: This Program is designed to be a template for all future programs in the class\n";
+    cout<<"Designed by: Red Hills Industries\n";
+    cout<< "********************\n";
+    return 0; 
+}
