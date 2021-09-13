01b70c88d9d45690c25e4993fc6243a1b8b68898
Conner Oaks <conoaks@gmail.com>
2021-09-12 22:20:02 -0400

    Lab 2 early stages

---
+++ lab_2.cpp
@@ -0,0 +1,42 @@
+//Conner Oaks 9/12/2021 Lab 2
+#include<iostream>
+using namespace std;
+
+void welcomemessage();
+int seriesPositive();
+int main()
+{
+    //Welcome Message
+    welcomemessage();
+    //Starting Normal execution
+    seriesPositive();
+}
+void welcomemessage()
+{
+    cout<< "********************\n";
+    cout<<"Welcome to the Lab_1 Program\n";
+    cout<<"Program Description: This program \n";
+    cout<<"Designed by: Red Hills Industries\n";
+    cout<< "********************\n";
+}
+int seriesPositive(int number, int count)
+{
+    cout<<"Please enter 10 positive numbers that are not 0, once you have reached 10 numbers, enter the number 0 to end.\n";
+    number=1;
+    while (number>=1)
+    {
+        cin>>number;
+        if(number>0)
+        {
+            cout<< "Number is invalid please enter a positive non zero number\n";
+            cin>>number;
+        }
+        if(number==0)
+            {
+                return count;
+            }
+        count++;
+    }
+}
+    
+    
