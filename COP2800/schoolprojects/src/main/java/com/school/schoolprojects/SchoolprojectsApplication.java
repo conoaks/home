4ac8d4bd1f345c9a7eab4647b4d1cd567e099a64
Conner Oaks <60011807+conoaks@users.noreply.github.com>
2022-11-09 20:05:05 +0000

    Java

---
+++ COP2800/schoolprojects/src/main/java/com/school/schoolprojects/SchoolprojectsApplication.java
@@ -0,0 +1,13 @@
+package com.school.schoolprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolprojectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolprojectsApplication.class, args);
+	}
+
+}
---
+++ COP2800/schoolprojects/src/test/java/com/school/schoolprojects/SchoolprojectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.schoolprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolprojectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
