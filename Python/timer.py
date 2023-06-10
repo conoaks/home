7691a7043118a8e659e9614a771804644eb53002
Conner Oaks <conoaks@gmail.com>
2023-06-10 14:17:04 +0000

    Timer

--- Python/timer.py
+++ Python/timer.py
@@ -1,5 +1,19 @@
 def main():
-    print("Hello, world!")
-
-if __name__ == "__main__":
-    main()
+    print()
+    timeMin = input("How many minutes would you like to set the timer for?")
+    timeSec = input("How many seconds would you like to set the timer for?")
+    timeLeftMin= int(timeMin)
+    timeLeftSec= int(timeSec)
+    while timeLeftSec > 0 and timeLeftMin > 0:
+            print(timeLeftMin, ":", timeLeftSec)
+            if timeLeftSec > 60:
+                    timeLeftSec = int(60)
+                    timeLeftSec = timeLeftSec - 1
+            else:
+                    timeLeftSec = timeLeftSec - 1
+            if timeLeftMin > 0 and timeLeftSec == 0:
+                    timeLeftMin = timeLeftMin - 1
+                    timeLeftSec = int(60)
+    if timeLeftMin ==0 and timeLeftSec == 0:
+        print("Time is up!")
+main()
