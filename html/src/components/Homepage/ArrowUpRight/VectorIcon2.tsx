138a4c6224de22d90cc0b556999836c35726f06f
conoaks <conoaks@gmail.com>
2024-02-16 00:24:37 +0000

    update webpage

--- html/src/components/Homepage/ArrowUpRight/VectorIcon2.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const VectorIcon2 = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 10 10' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <path d='M0.25 0H10V9.75' stroke='#121314' strokeWidth={1.5} strokeLinecap='round' strokeLinejoin='round' />
-  </svg>
-);
-const Memo = memo(VectorIcon2);
-export { Memo as VectorIcon2 };
--- html/src/components/Homepage/VectorIcon5.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const VectorIcon5 = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 12 12' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <path d='M0 12L12 0' stroke='#121314' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' />
-  </svg>
-);
-const Memo = memo(VectorIcon5);
-export { Memo as VectorIcon5 };
